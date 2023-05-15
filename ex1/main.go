1c7a4afc9e38a1b0aba1fca771f56f4299e86446
nekidb <nekidb@gmail.com>
2023-05-15 23:03:05 +0300

    ex1 solution

--- ex1/main.go
+++ ex1/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+type Human struct {
+	Height int
+	Age    int
+}
+
+// Для встраивания структуры Human в структуру Action, указываем Human как поле, но без имени.
+type Action struct {
+	Human
+	ActionField string
+}
 
 func main() {
-	fmt.Println("Hello")
 }
