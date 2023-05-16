618960f2d1891a2b98d18f71ee855fb6b1eef5fe
nekidb <nekidb@gmail.com>
2023-05-16 22:32:42 +0300

    ex3 solution

--- ex3/main.go
+++ ex3/main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+// функция считает квадрат числа
+func square(n int) int {
+	return n * n
+}
+
+// функция конкурентно вычисляет квадраты чисел из nums и записывае их в канал out
+func squaresSender(nums []int, out chan<- int) {
+	// создаем wg, чтобы можно было понять когда завершаться все горутины, вычисляющие квадраты
+	wg := sync.WaitGroup{}
+
+	// для каждого числа из num создаем горутину, которая вычисляет квадрат числа и записывает
+	// результат в канал out
+	for _, num := range nums {
+		// перед созданием горутины, увеличиваем счетчик в wg
+		wg.Add(1)
+		go func(n int) {
+			// по окончанию выполнения функции уменьшаем счетчик в wg
+			wg.Done()
+			// передаем в канал квадрат числа
+			out <- square(n)
+		}(num)
+	}
+	// ожидаем завершения всех горутин
+	wg.Wait()
+	// закрываем канал
+	close(out)
+}
+
+func main() {
+	nums := []int{2, 4, 6, 8, 10}
+
+	squared := make(chan int)
+
+	go squaresSender(nums, squared)
+
+	sum := 0
+	// читаем из канала и записываем результат в sum
+	// когда канал закроется, тогда завершится цикл
+	for s := range squared {
+		sum += s
+	}
+
+	fmt.Println(sum)
 }
