acb475846dc95f1cbb9ad23369f6c6f5a9d14de5
nekidb <nekidb@gmail.com>
2023-05-18 20:00:24 +0300

    ex5 solution

--- ex5/main.go
+++ ex5/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"math/rand"
+	"time"
+)
 
+// функция бесконечно отправляет рандомные целые числа в канал out
+func writeToChan(out chan<- int) {
+	for {
+		out <- rand.Int()
+	}
+}
+
+// функция бесконечно читает из канала in целые числа
+func readFromChan(in <-chan int) {
+	for {
+		<-in
+	}
+}
+
+func main() {
+	N := 5
+	ch := make(chan int)
+
+	// создаем горутину, которая отправляет значения в канал
+	go writeToChan(ch)
+
+	// создаем горутину, которая читает значения из канала
+	go readFromChan(ch)
+
+	// ждем N секунд и завершаем программу
+	time.Sleep(time.Duration(N) * time.Second)
 }
