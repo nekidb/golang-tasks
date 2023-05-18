ae289416715ec6f03bd9c95ad2db26a545764cdf
nekidb <nekidb@gmail.com>
2023-05-18 20:40:46 +0300

    ex8 solution

--- ex8/main.go
+++ ex8/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+// Когда бит нужно поменять на 0, мы будем применять f1. Когда на 1, тогда f2
+// x | y | f1 | f2|
+// 0 | 0 | 0  | 0 |
+// 0 | 1 | 0  | 1 |
+// 1 | 0 | 1  | 1 |
+// 1 | 1 | 0  | 1 |
+
+// Получаем формулы:
+// f1 = X & ^Y
+// f2 = (^X & Y) || (X & ^Y) || (X & Y)
+
+// функция меняет в числе num бит с номером bit на значение val
+func changeBit(num, bit, val int64) int64 {
+	m := int64(1 << bit)
+	if val == 0 {
+		return num & ^m
+	}
+	return (^num & m) | (num & ^m) | (num & m)
+}
+
+func main() {
+	number := int64(123456789)
+
+	fmt.Printf("Исходное число:       %b\n", number)
+	fmt.Printf("Меняем 3-ый бит на 1: %b\n", changeBit(number, 3, 1))
+	fmt.Printf("Меняем 2-ый бит на 0: %b\n", changeBit(number, 2, 0))
 }
