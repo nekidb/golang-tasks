e5d440318f76a9b969c52d7ddd0d1c1bd36609bc
nekidb <nekidb@gmail.com>
2023-05-20 21:41:13 +0300

    ex19 solution

--- ex19/main.go
+++ ex19/main.go
@@ -1,5 +1,26 @@
 package main
 
-  func main() {
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
-  }
+// функция переворачивает строку
+func revert(s string) string {
+	// создаем слайс рун, который будет содержать перевернутую строку
+	runes := make([]rune, utf8.RuneCountInString(s))
+	idx := 1
+	// последовательно берем символы строки и с конца заполняем массив runes
+	for _, r := range s {
+		runes[len(runes)-idx] = r
+		idx++
+	}
+
+	return string(runes)
+}
+
+func main() {
+	s := "Ёлка"
+
+	fmt.Println(revert(s))
+}
