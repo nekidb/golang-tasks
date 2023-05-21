4c10181d0ea20bef51d95d21e8b93fa3b76c1f45
nekidb <nekidb@gmail.com>
2023-05-21 20:40:15 +0300

    ex9 refactored

--- ex9/main.go
+++ ex9/main.go
@@ -2,48 +2,47 @@ package main
 
 import "fmt"
 
-// функция отправляет числа из nums в канал out
-func f1(nums []int, out chan<- int) {
-	for _, num := range nums {
-		out <- num
-	}
-	close(out)
+func f1(nums []int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for _, num := range nums {
+			out <- num
+		}
+	}()
+
+	return out
 }
 
-// функция читает числа из канала in, умножает их на 2 и отправляет в канал out
-func f2(in <-chan int, out chan<- int) {
-	for num := range in {
-		out <- num * 2
-	}
-	close(out)
+func f2(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for num := range in {
+			out <- num * 2
+		}
+	}()
+
+	return out
 }
 
-// функция читает числа из канала in и выводит в stdout
-// по завершению выполнения отправляем в канал done
-func f3(done chan<- struct{}, in <-chan int) {
-	for num := range in {
-		fmt.Println(num)
-	}
-	done <- struct{}{}
+func f3(in <-chan int) <-chan struct{} {
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		for num := range in {
+			fmt.Println(num)
+		}
+	}()
+
+	return done
 }
 
 func main() {
-	// запускаем 3 горутины:
-	//  первая пишет в канал ch1
-	//  вторая читает из канала ch1 и отправляет в ch2 удвоенное число
-	//  третья читает из ch2 и выводит в stdout
-	// чтобы горутина main не завершилась, читаем из канала done
 	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-
-	go f1(nums, ch1)
-
-	go f2(ch1, ch2)
-
-	done := make(chan struct{})
-	go f3(done, ch2)
-
+	ch1 := f1(nums)
+	ch2 := f2(ch1)
+	done := f3(ch2)
 	<-done
 }
