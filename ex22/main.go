5b671b92f6ca4b602369869f7e2dd9bc5f1d67ee
nekidb <nekidb@gmail.com>
2023-05-22 21:00:13 +0300

    ex22 solution

--- ex22/main.go
+++ ex22/main.go
@@ -1,5 +1,35 @@
 package main
 
-  func main() {
+import (
+	"fmt"
+	"math/big"
+)
 
-  }
+func main() {
+	// создаем два числа больше 2^20
+	a := new(big.Int)
+	a.SetString("555_555_555_555_555_555_555_555", 0)
+	b := new(big.Int)
+	b.SetString("222_222_222_222_222_222_222_222", 0)
+
+	// находим сумму
+	sum := new(big.Int)
+	sum.Add(a, b)
+	// находим разность
+	sub := new(big.Int)
+	sub.Sub(a, b)
+	// находим деление
+	div := new(big.Int)
+	div.Div(a, b)
+	// находим произведение
+	mul := new(big.Int)
+	mul.Mul(a, b)
+
+	fmt.Println("a:   ", a)
+	fmt.Println("b:   ", b)
+	fmt.Println("Sum: ", sum)
+	fmt.Println("Sub: ", sub)
+	fmt.Println("Div: ", div)
+	fmt.Println("Mul: ", mul)
+
+}
