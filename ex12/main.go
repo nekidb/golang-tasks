582d3de137795125739c816504987dd2edd1c50d
nekidb <nekidb@gmail.com>
2023-05-22 20:29:21 +0300

    ex12 solution

--- ex12/main.go
+++ ex12/main.go
@@ -1,5 +1,36 @@
 package main
 
-  func main() {
+import (
+	"fmt"
+	"strings"
+)
 
-  }
+type Set map[string]struct{}
+
+func (s Set) Add(val string) {
+	s[val] = struct{}{}
+}
+
+func (s Set) String() string {
+	out := strings.Builder{}
+
+	out.WriteString("{ ")
+	for k, _ := range s {
+		out.WriteString(k)
+		out.WriteString(" ")
+	}
+	out.WriteString("}")
+
+	return out.String()
+}
+
+func main() {
+	someStrings := []string{"cat", "cat", "dog", "cat", "tree"}
+
+	s := Set{}
+	for _, str := range someStrings {
+		s.Add(str)
+	}
+
+	fmt.Println(s)
+}
