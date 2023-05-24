bacedf6d69e210604d6bfc26cde23c3f823b9aed
nekidb <nekidb@gmail.com>
2023-05-24 14:32:41 +0300

    ex15 solution

--- ex15/main.go
+++ ex15/main.go
@@ -1,5 +1,13 @@
 package main
 
-  func main() {
+var justString string
 
-  }
+func someFunc() {
+	// вместо того, чтобы записывать результат функции createHugeString в промежуточную переменную
+	// мы записываем "обрезанный" результат сразу в переменную justString
+	justString = createHugeString()[:100]
+}
+
+func main() {
+	someFunc()
+}
