111182ad8e56de527194f06e2f856202be6593e8
nekidb <nekidb@gmail.com>
2023-06-01 17:07:08 +0300

    ex3/solution2 some changes

--- ex3/solution1/main.go
+++ ex3/solution1/main.go
@@ -5,44 +5,39 @@ import (
 	"sync"
 )
 
-// функция считает квадрат числа
 func square(n int) int {
 	return n * n
 }
 
-// функция конкурентно вычисляет квадраты чисел из nums и записывае их в канал out
-func squaresSender(nums []int, out chan<- int) {
-	// создаем wg, чтобы можно было понять когда завершаться все горутины, вычисляющие квадраты
-	wg := sync.WaitGroup{}
+func squaresSender(nums []int) <-chan int {
+	out := make(chan int)
 
-	// для каждого числа из num создаем горутину, которая вычисляет квадрат числа и записывает
-	// результат в канал out
-	for _, num := range nums {
-		// перед созданием горутины, увеличиваем счетчик в wg
-		wg.Add(1)
-		go func(n int) {
-			// по окончанию выполнения функции уменьшаем счетчик в wg
-			wg.Done()
-			// передаем в канал квадрат числа
-			out <- square(n)
-		}(num)
-	}
-	// ожидаем завершения всех горутин
-	wg.Wait()
-	// закрываем канал
-	close(out)
+	go func() {
+		defer close(out)
+
+		var wg sync.WaitGroup
+		wg.Add(len(nums))
+
+		for _, num := range nums {
+			go func(n int) {
+				wg.Done()
+
+				out <- square(n)
+			}(num)
+		}
+
+		wg.Wait()
+	}()
+
+	return out
 }
 
 func main() {
 	nums := []int{2, 4, 6, 8, 10}
 
-	squared := make(chan int)
+	squared := squaresSender(nums)
 
-	go squaresSender(nums, squared)
-
-	sum := 0
-	// читаем из канала и записываем результат в sum
-	// когда канал закроется, тогда завершится цикл
+	var sum int
 	for s := range squared {
 		sum += s
 	}
